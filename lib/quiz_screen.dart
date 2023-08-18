022b93ae4f38359192bed5630f239ceefce5a747
SandaruwanWGMA <molinduachintha24@gmail.com>
2023-08-18 13:16:54 +0530

    commit

--- lib/quiz_screen.dart
+++ lib/quiz_screen.dart
@@ -1,10 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_quiz_app/Data/questions.dart';
 
-class QuizScreen extends StatelessWidget {
+class QuizScreen extends StatefulWidget {
   const QuizScreen({super.key});
 
   @override
+  State<QuizScreen> createState() {
+    return _QuizScreenState();
+  }
+}
+
+class _QuizScreenState extends State<QuizScreen> {
+  int questionNumber = 0;
+
+  void stepUpQuestionNumber() {
+    setState(() {
+      questionNumber++;
+    });
+  }
+
+  @override
   Widget build(context) {
-    return const Text("Question text");
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          questions[questionNumber].question,
+          textAlign: TextAlign.center,
+        ),
+        ...questions[questionNumber].answers.map((answer) => TextButton(
+              onPressed: stepUpQuestionNumber,
+              child: Text(answer),
+            )),
+      ],
+    );
   }
 }
