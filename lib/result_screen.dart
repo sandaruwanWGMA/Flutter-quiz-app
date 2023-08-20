6aa4e9a8668b5e26a3f5e18e8aa1d18d5c9f9cb3
SandaruwanWGMA <molinduachintha24@gmail.com>
2023-08-20 09:10:28 +0530

    commit

--- lib/result_screen.dart
+++ lib/result_screen.dart
@@ -24,56 +24,89 @@ class _ResultScreenState extends State<ResultScreen> {
     if (correctAnswers <= (totalQuestions * 0.5).toInt()) {
       comment = "Below Average";
       color = Colors.red;
-    }
-
-    if (correctAnswers <= (totalQuestions * 0.75).toInt()) {
+    } else if (correctAnswers <= (totalQuestions * 0.75).toInt()) {
       comment = "Average";
       color = Colors.green;
-    }
-
-    if (correctAnswers > (totalQuestions * 0.75).toInt()) {
+    } else if (correctAnswers > (totalQuestions * 0.75).toInt()) {
       comment = "Good";
       color = Colors.blue;
     }
     return MaterialApp(
       home: Scaffold(
-        body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Center(
-                child: Text(
-                  "Out of $totalQuestions questions, $correctAnswers are correct",
-                  style: const TextStyle(
-                    fontSize: 24,
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 36,
-              ),
-              Center(
-                child: Text(
-                  comment,
-                  style: TextStyle(
-                    color: color,
-                    fontSize: 54,
-                    fontWeight: FontWeight.w700,
-                  ),
-                ),
-              ),
-              TextButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const Quiz(),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 255, 255, 255),
+                Color.fromARGB(255, 227, 245, 236),
+                Color.fromARGB(255, 201, 200, 200)
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Center(
+                  child: Text(
+                    "Out of $totalQuestions questions, $correctAnswers are correct",
+                    style: const TextStyle(
+                      fontSize: 24,
                     ),
-                  );
-                },
-                child: const Text("Restart quiz"),
-              ),
-            ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 36,
+                ),
+                Center(
+                  child: Text(
+                    comment,
+                    style: TextStyle(
+                      color: color,
+                      fontSize: 54,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ),
+                TextButton.icon(
+                  style: TextButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 24,
+                      vertical: 12,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(
+                          15), // Adjust the radius as needed
+                    ),
+                    backgroundColor: const Color.fromARGB(15, 158, 158, 158),
+                    foregroundColor: Colors.orange,
+                    side: const BorderSide(
+                      width: 0.5,
+                      color: Color.fromARGB(212, 255, 153, 0),
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const Quiz(),
+                      ),
+                    );
+                  },
+                  icon: const Icon(
+                    Icons.restart_alt,
+                  ),
+                  label: const Text(
+                    "Restart quiz",
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
